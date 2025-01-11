01ccbe520518f52706bdb7c79cc9c230ad10395e
Berkay Gök <berkaybugragok@gmail.com>
2025-01-12 02:33:22 +0300

    jupyter notebook added for EDA

--- mushroom/main.py
+++ mushroom/main.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
-mushroom_data = pd.read_csv("./resources/mushrooms.csv")
+class MushroomUtils:
+    def __init__(self, csv_path):
+        self.data = pd.read_csv(csv_path)
 
-print(mushroom_data.head())
+    def get_labeled_data(self):
+        """
+        Labels the data using LabelEncoder, useful for tree-based models
+        """
+        label_encoder = LabelEncoder()
+        labeled_data = self.data.copy()
+        for column in labeled_data.columns:
+            labeled_data[column] = label_encoder.fit_transform(labeled_data[column])
+        return labeled_data
+
+
+
+if __name__ == "__main__":
+    processor = MushroomUtils('./resources/mushrooms.csv')
+    processed_data = processor.get_labeled_data()
+    print(processed_data.head())
