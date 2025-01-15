d2a69ac27efab123895e023e2bfa93f71d1efafb
Berkay Gök <berkaybugragok@gmail.com>
2025-01-15 20:02:58 +0300

    ssl added, contact page modified

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ This is the project repository for my portfolio website.
 
 It features my data science projects, and the challenge was to gain experience with web development and deployment.
 
-The website will be accessible soon. 
+***The website is accessible at:***
+
+www.berkaybgk.site 
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -141,3 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security settings
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
