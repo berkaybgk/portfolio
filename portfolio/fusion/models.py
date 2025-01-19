424a06e333881c462093be3a998457e99492425e
Berkay Gök <berkaybugragok@gmail.com>
2025-01-20 01:39:03 +0300

    trigger main

--- portfolio/fusion/models.py
+++ portfolio/fusion/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Chat model4
+# Chat model5
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link the chat to a user
     message = models.TextField()  # User's message
