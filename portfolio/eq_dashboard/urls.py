ac673a623af170504dba127454e912f00140427d
Berkay Gök <berkaybugragok@gmail.com>
2025-01-27 21:15:19 +0300

    earthquake dashboard integrated

--- portfolio/eq_dashboard/urls.py
+++ portfolio/eq_dashboard/urls.py
@@ -3,5 +3,5 @@ from .views import EarthquakeDashboardView, EarthquakeDataAPI
 
 urlpatterns = [
     path('eq-dashboard/', EarthquakeDashboardView.as_view(), name='eq_dashboard'),
-    path('eq-dashboard/api/data/', EarthquakeDataAPI.as_view(), name='eq_dashboard'),
+    path('eq-dashboard/api/data/', EarthquakeDataAPI.as_view(), name='eq_dashboard_api'),
 ]
